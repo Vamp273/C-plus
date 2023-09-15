201ed9f559f1703fe7044a34c98af45dcda177aa
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-09-15 14:50:38 +0530

    enum

---
+++ class color.c
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class color{
+    public:
+    colorenum{
+      black,
+      blue,
+      red
+    };
+
+};
+
+int main(){
+    color::colorenum obj;
+    cout<<sizeof(obj)<<endl
+
+
+}
