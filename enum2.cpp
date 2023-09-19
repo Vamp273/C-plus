c02e0c1efc943ef4e8a0e5fe03e8f4719d0a1a16
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-09-19 19:22:57 +0530

    enum ques

---
+++ enum2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+using namespace std;  
+enum season { spring = 34, summer = 4, autumn = 9, winter = 32};  
+int main()  
+{  
+    season S;  
+    S = summer;  
+    cout << "Summer : " << S <<endl;  
+    return 0;  
+}
