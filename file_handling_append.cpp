b88b210c022f44a942e7275c5e256d0f31a03652
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-09-27 14:28:14 +0530

    writing content in the file.

---
+++ file_handling_append.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    char s[30];
+    ofstream fileout;
+    ifstream filein;
+    fileout.open("data", ios::app);
+    fileout<<"hello world";
+    fileout.close();
+
+    filein.open("data", ios::in);
+    filein.getline(s,30);
+    cout<<s;
+
+}
