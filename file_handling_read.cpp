a52c0a960788ba015f97f15932541c98d8e1f019
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-10-09 15:26:47 +0530

    reading of file

---
+++ file_handling_read.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    char s[10];
+    ifstream inf("result.txt");
+    inf>>s;
+    inf.close();
+    cout <<5;
+    return 0;
+}
