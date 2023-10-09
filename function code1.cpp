22ec7562548d34c7fd38dd1d73b599cebf61d72e
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-10-09 15:29:09 +0530

    functions ques

---
+++ function code1.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+//
+//class Student
+//{
+//    int id;
+//    char name[20];
+//    int reg_no;
+//    
+//    public:
+//    	void readdata();
+//    	void outputdata();
+//};
+//
+//void Student:: readdata(){
+//    
+//    cout<<"Enter your name : ";
+//    cin>>name;
+//    cout<<"Enter your id : ";
+//    cin>>id;
+//    cout<<"Enter your reg. no. : ";
+//    cin>>reg_no;
+//}
+//
+//void Student:: outputdata(){
+//	
+//	cout<<"name = "<<name<<endl;
+//    cout<<"id = "<<id<<endl;
+//    cout<<"registration number ="<<reg_no<<endl;   
+//}
+//
+//int main()
+//{
+//    Student s;
+//    s.readdata();
+//    s.outputdata();
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	cout <<"a";
+//	cout << "b"<<endl;
+//	cout<< "c" <<endl;
+//}
+
+
+int main()
+{
+	
+	double f = 3.1456788;
+	cout<< setprecision(5)<<f<<"\n";
+	cout << setprecision(9)<< f<<"\n";
+	cout << fixed;
+	cout <<setprecision(5)<<f<<"\n";
+	cout <<setprecision(9)<<f<<"\n";
+	return 0;
+}
+
+
+
+
+
