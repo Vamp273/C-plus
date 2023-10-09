fea8e349c3520e3f057730b45d31a2bf72b0d737
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-10-09 15:24:14 +0530

    conversion of class datatype to basic

---
+++ class_to_basic_converse.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    private:
+    int rollno;
+    float fees;
+    public:
+    student(int a,float m)
+    {
+        rollno=a;
+        fees=m;
+    }
+    operator int()
+    {
+        return(rollno);
+    }
+    operator float()
+    {
+        return(fees);
+    }
+};
+int main()
+{
+    int j;
+    float f;
+    student st(5,4200.50);
+    j=st;
+    f=st;
+    cout<<"\nvalue of j:"<<j<<"\n";
+    cout<<"\nvalue of f:"<<f<<"\n";
+    return 0;
+}
