51ed0a1a186ef2e1b2457e8ed47bfd07bbfa93e0
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2023-10-10 23:31:05 +0530

    lecture 2

---
+++ Lec2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1 = 70;
+	double num2 = 256.786;
+	char ch='A';
+	cout<<num1<<endl;  // endl is used for next line
+	cout<<num2<<endl;
+	cout<<"character : "<<ch<<endl;
+}
